d6ba2501daedc7564ecabcc9c9645ad14a07cf48
Sinan Bozkuş <sinan.bozkus@gmail.com>
2019-05-18 12:59:32 +0300

    Debug mode added, PascalCase Result added for callback function.

--- src/FormHelper/Types/FormHelperConfiguration.cs
+++ src/FormHelper/Types/FormHelperConfiguration.cs
@@ -5,5 +5,6 @@
         public bool UsePascalCaseJson { get; set; } = false;
         public string CheckTheFormFieldsMessage { get; set; } = "Check the form fields.";
         public int RedirectDelay { get; set; } = 2000;
+        public bool DebugMode { get; set; } = false;
     }
 }
